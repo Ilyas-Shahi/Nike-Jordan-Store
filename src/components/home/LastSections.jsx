efe7d80fbd5e81e571833fe0ada29946f0f2672d
Ilyas <ilyasm.shahi359@gmail.com>
2023-02-04 23:29:34 +0500

    Minor fixes

--- src/components/home/LastSections.jsx
+++ src/components/home/LastSections.jsx
@@ -13,24 +13,24 @@ import JordanElevatedImg from '../../assets/jordan-elevated.jpg';
 const LastSections = () => {
   return (
     <>
-      <div className="p-12">
+      <div className="p-12 w-full">
         <h3 className="mb-6 text-2xl font-extrabold">
           JORDAN GEAR FOR THE ENTIRE FAM
         </h3>
 
-        <div className="flex gap-3">
-          <div>
-            <img src={JordanMenImg} alt="" />
+        <div className="flex gap-3 w-full">
+          <div className="w-full">
+            <img src={JordanMenImg} alt="" className="w-full" />
             <h3 className="mt-12 mb-7 text-2xl font-extrabold">JORDAN MEN</h3>
             <ButtonPrimary>Shop</ButtonPrimary>
           </div>
-          <div>
-            <img src={JordanWomenImg} alt="" />
+          <div className="w-full">
+            <img src={JordanWomenImg} alt="" className="w-full" />
             <h3 className="mt-12 mb-7 text-2xl font-extrabold">JORDAN WOMEN</h3>
             <ButtonPrimary>Shop</ButtonPrimary>
           </div>
-          <div>
-            <img src={JordanKidsImg} alt="" />
+          <div className="w-full">
+            <img src={JordanKidsImg} alt="" className="w-full" />
             <h3 className="mt-12 mb-7 text-2xl font-extrabold">JORDAN KIDS</h3>
             <ButtonPrimary>Shop</ButtonPrimary>
           </div>
@@ -41,32 +41,26 @@ const LastSections = () => {
         <h3 className="mb-6 text-2xl font-extrabold">THE LATEST FROM JORDAN</h3>
 
         <div className="flex gap-3">
-          <Link to="#" className="h-1/2 w-full">
-            <div
-              style={{ backgroundImage: `url(${SkyJordanImg})` }}
-              className="h-[750px] w-full  bg-no-repeat flex items-end bg-cover bg-center p-12"
-            >
-              <div>
-                <h4 className="mb-6 text-2xl leading-6 font-extrabold">
-                  JORDAN 23/7 <br /> 'SKY JORDAN'
-                </h4>
-                <ButtonPrimary>Shop</ButtonPrimary>
-              </div>
+          <Link to="#" className="w-full relative">
+            <div className="absolute z-10 bottom-12 left-12">
+              <h4 className="mb-6 text-2xl leading-6 font-extrabold z-10">
+                JORDAN 23/7 <br /> 'SKY JORDAN'
+              </h4>
+              <ButtonPrimary>Shop</ButtonPrimary>
             </div>
+
+            <img src={SkyJordanImg} alt="" className="w-full" />
           </Link>
 
-          <Link to="#" className="h-1/2 w-full">
-            <div
-              style={{ backgroundImage: `url(${JordanElevatedImg})` }}
-              className="h-[750px] w-full  bg-no-repeat flex items-end bg-cover bg-center p-12"
-            >
-              <div>
-                <h4 className="mb-6 text-2xl text-white font-extrabold">
-                  ELEVATED EVERYDAY ESSENTIALS
-                </h4>
-                <ButtonSecondary>Shop</ButtonSecondary>
-              </div>
+          <Link to="#" className="w-full relative">
+            <div className="absolute z-10 bottom-12 left-12">
+              <h4 className="mb-6 text-2xl text-white font-extrabold">
+                ELEVATED EVERYDAY ESSENTIALS
+              </h4>
+              <ButtonSecondary>Shop</ButtonSecondary>
             </div>
+
+            <img src={JordanElevatedImg} alt="" className="w-full" />
           </Link>
         </div>
       </div>
